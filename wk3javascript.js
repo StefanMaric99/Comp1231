cf31425e2587e18086c40c034f8bd37bc6cb343d
StefanMaric99 <57594996+StefanMaric99@users.noreply.github.com>
2020-01-21 09:48:39 -0500

    Add files via upload

---
+++ wk3javascript.js
@@ -0,0 +1,47 @@
+/*
+Stefan Maric
+
+initial practice warm up
+
+let x = prompt("please enter your age:");
+
+
+
+if (x >= 19) {
+    document.write("Age verified, your the legal age to buy beer.");
+
+} else {
+    document.write("Your under the legal age and will not be permitted to purchase alcohol.");
+}
+*/
+
+/*
+const areWeInSchool = prompt("Are you in school:" );
+
+if (areWeInSchool ==='yes' && areWeInSchool === 'Yes') {
+
+    console.log("Your in school");
+    document.write("Your in school");
+} else if (areWeInSchool === 'No' && areWeInSchool === 'no') {
+    console.log("Your not in school");
+    document.write("Your not in school");
+}
+
+*/
+
+//let y = Math.cbrt(54);
+
+let classTime = Math.Floor(Math.randum() * 100);
+
+if (classTime) {
+    log(classTime, 'is less than 25')
+} else if(classTime) {
+    log(classTime, 'is less than 50')
+} else if (classTime) {
+    log(classTime)
+}
+
+
+function log(num) {
+    console.log('The number is', num);
+}
