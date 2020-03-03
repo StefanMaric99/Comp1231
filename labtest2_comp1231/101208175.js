19a7ff380ef579b52439cc2a2fcb901c37faec12
StefanMaric99 <57594996+StefanMaric99@users.noreply.github.com>
2020-03-03 09:37:35 -0500

    Add files via upload

---
+++ labtest2_comp1231/101208175.js
@@ -0,0 +1,41 @@
+//Stefan Maric - 101208175
+//labtest2
+//comp1231
+
+    let initArr = [];
+
+    let numArr = [123,3,64,3];
+    let stringArr = ['yo','test','words','green'];
+    let thirdArr = [144,2,5,7];
+    initArr.push(numArr,stringArr,thirdArr);
+
+    console.log(initArr);
+
+    let s;
+
+        initArr.forEach(function(innerArrays, index) {
+            innerArrays.forEach(function(element,index){
+                console.log(element)
+            })
+
+        });
+        // s.toString();
+        // s = s.concat(numArr,stringArr,thirdArr);
+        // let strings= '';
+        // strings = s;
+        //
+        // console.log(strings);
+
+
+        function arrayToString(arr) {
+            let str = '';
+            arr.forEach(function(i, index) {
+                str += i;
+                if (index != (arr.length - 1)) {
+                    str += ',';
+                };
+            });
+            s=str;
+            return str;
+        }
+        console.log(arrayToString(initArr));
